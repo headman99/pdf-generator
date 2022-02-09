485d4de4bde03a87db77fa333fa04d576888480c
Davide Capacchione <davide.capacchione.58@gmail.com>
2022-02-09 17:34:48 +0100

    drag and drop final modify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState,useRef } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 import PdfPages from './components/PdfPages';
 import '../src/CSS/App.css'
 import { LazyDownloadPDFButton } from './components/LazyDownloadPDFButton ';
@@ -14,23 +14,22 @@ function App() {
   const [qrCodeUrl, setQrCodeUrl] = useState([]);
   const [isSelectedFile, setIsSelectedFile] = useState(null);
   const { width, height } = useWindowSize()
-  const [selectedFileName,setSelectedFileName] = useState('')
+  const [selectedFileName, setSelectedFileName] = useState('')
   const inputFile = useRef();
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: '.xls',
     onDrop: useCallback((file) => {
-      if (file!==undefined && file[0].name.split('.').pop() === 'xls') {
-        setSelectedFileName(file[0].name)
-        inputFile.current.value=''
+      if (file !== undefined && file[0].name.split('.').pop() === 'xls') {
+        inputFile.current.value = '';
         readExcel(file[0]).then((data) => { setExcelData(data) })
         setQrCodeUrl([])
         setIsSelectedFile(true)
+        setSelectedFileName(file[0].name)
       } else {
         setIsSelectedFile(false)
       }
-
-    }, [])
+    }, []),
   })
 
   const SaveQrCodeUrl = () => {
@@ -42,43 +41,57 @@ function App() {
   }
 
   return (
-    <div id="container" style={{ width: width, height: height, minHeight: '100%', minWidth: '100%',display:'flex',flexDirection:'column' }}>
-      <div id="btn-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 180, minWidth: 1000, height: '20%', width: '100%' }}>
-        <input disabled={!isSelectedFile} className='btn' value={show ? 'Nascondi anteprima Pdf' : 'Mostra anteprima Pdf'} type='button' onClick={() => {
-          if (isSelectedFile === true) {
-            if (qrCodeUrl.length < 1)
-              SaveQrCodeUrl()
-            setShow(!show)
-          }
-        }} />
-        <input ref={inputFile} id="selector" className='btn' type="file" accept='.xls' color='blue' onChange={(e) => {
-          const file = e.target?.files[0];
-          if (file!==undefined && file.name.split('.').pop() === 'xls') {
-            setSelectedFileName(file.name)
-            readExcel(file).then((data) => { setExcelData(data) })
-            setQrCodeUrl([])
-            setIsSelectedFile(true)
-          } else {
-            setIsSelectedFile(false)
-          }
-        }} />
-        <LazyDownloadPDFButton SaveQrCodeUrl={SaveQrCodeUrl} disabled={!isSelectedFile} exceldata={excelData} />
-      </div>
-      <div id = "fileTitle">
-        <h3>{selectedFileName}</h3>
-      </div>
-      <div
-        id="main-content"
-        style={{ borderTopWidth: 2, borderTopStyle: 'solid', borderColor: 'gray', height: '78%', width: '100%' }}
-      >
-        <div id='dropzone' {...getRootProps()}>
-          <input  {...getInputProps()} />
-          <div id="curtain" style={isDragActive?{opacity:0.4,backgroundColor:'lightgray',zIndex:5}:(isSelectedFile?{opacity:0}:null)}>
+    <div id="container" style={{ width: width, height: height, minHeight: '100%', minWidth: '100%', display: 'flex', flexDirection: 'column' }}>
+      <div id="btn-container">
+        <div style={{ display: 'flex', flexDirection: 'column', height: '80%', marginRight: 20, width: '30%' }}>
+          <div className='inputBtn bottone'>
+            <input ref={inputFile} style={{ color: 'white', fontFamily: 'inherit',fontSize:15 }} id="selector" type="file" accept='.xls' color='blue' onChange={(e) => {
+              const file = e.target?.files[0];
+              if (file !== undefined && file.name.split('.').pop() === 'xls') {
+                readExcel(file).then((data) => { setExcelData(data) })
+                setQrCodeUrl([])
+                setIsSelectedFile(true)
+                setSelectedFileName(file.name)
+              } else {
+                setIsSelectedFile(false)
+              }
+            }} />
+          </div>
+          <div id='dropzone' {...getRootProps()} style={isDragActive ? { backgroundColor: 'lightgray' } : null}>
+            <input  {...getInputProps()} />
             {isDragActive ? <p>Rilascia</p> : <p>Trascina qui il file</p>}
           </div>
-          {isSelectedFile === false && <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}><span>Seleziona un file con estensione: ".xls"</span></div>}
-          {show ? <PdfPages exceldata={excelData} qrCodeUrl={qrCodeUrl} /> : (isSelectedFile === true && <BodyTableRow excelData={excelData} />)}
         </div>
+        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: 20, justifyContent: 'space-between', height: '80%' }}>
+          <div className='bottone'>
+            <input disabled={!isSelectedFile} className='btn' value={show ? 'Nascondi anteprima Pdf' : 'Mostra anteprima Pdf'} type='button' onClick={() => {
+              if (isSelectedFile === true) {
+                if (qrCodeUrl.length < 1)
+                  SaveQrCodeUrl()
+                setShow(!show)
+              }
+            }} />
+          </div>
+          <LazyDownloadPDFButton SaveQrCodeUrl={SaveQrCodeUrl} disabled={!isSelectedFile} exceldata={excelData} />
+        </div>
+
+      </div>
+
+      <div
+        id="main-content"
+        style={{ borderTopWidth: 2, borderTopStyle: 'solid', borderColor: 'gray', height: '68%', width: '100%' }}
+      >
+        <div style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          {
+            (isSelectedFile === true && show === false) &&
+            <div id="fileTitle" >
+              <h3>{selectedFileName}</h3>
+            </div>
+          }
+
+        </div>
+        {isSelectedFile === false && <div style={{ width: '100%', display: 'flex', justifyContent: 'center', marginTop: '10%' }}>{<h3>Seleziona un file con estensione: ".xls"</h3>}</div>}
+        {show ? <PdfPages exceldata={excelData} qrCodeUrl={qrCodeUrl} /> : (isSelectedFile === true && <BodyTableRow excelData={excelData} />)}
       </div>
 
       <div id="footer" style={show ? { display: 'none' } : null}></div>
